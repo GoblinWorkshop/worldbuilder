226172af106fed158eedd730c5a6499234b29144
Casmo <mathieu@fellicht.nl>
2019-06-12 18:44:14 -0400

    Link to articles in app for brandname.

--- resources/views/nav/app.blade.php
+++ resources/views/nav/app.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <a class="navbar-brand" href="{{ url('/') }}">
+    <a class="navbar-brand" href="{{route('articles.index')}}">
         {{ config('app.name', 'Goblin Workshop') }}
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
