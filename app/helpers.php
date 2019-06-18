75fb2f14359d0e18acd58a6bc512c18736d8a0b5
Casmo <mathieu@fellicht.nl>
2019-06-17 21:21:41 -0400

    Fix html5 purifier attributes.

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,7 @@
 <?php
 if (!function_exists('html')) {
     function html($unsafeHtml) {
-        $config = \HTMLPurifier_Config::createDefault();
+        $config = HTMLPurifier_HTML5Config::createDefault();
         $config->set('Cache.SerializerPath', storage_path('framework/cache'));
         $HTMLPurifier = new \HTMLPurifier($config);
         return $HTMLPurifier->purify($unsafeHtml);
