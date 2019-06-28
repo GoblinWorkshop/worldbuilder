f938f498cf69d70e17df4de806d74d8e3937c5d8
Casmo <mathieu@fellicht.nl>
2019-06-28 09:56:56 -0400

    Recover serializerpath.

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,7 @@
 if (!function_exists('html')) {
     function html($unsafeHtml) {
         $config = HTMLPurifier_HTML5Config::createDefault();
+        $config->set('Cache.SerializerPath', storage_path('framework/cache'));
         $HTMLPurifier = new \HTMLPurifier($config);
         return $HTMLPurifier->purify($unsafeHtml);
     }
