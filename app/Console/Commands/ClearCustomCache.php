41e7ac7f8a63091cf9f151ac9e1e57c64ffc3e7f
Casmo <mathieu@fellicht.nl>
2019-07-05 11:38:58 -0400

    Clear custom cache through artisan

---
+++ app/Console/Commands/ClearCustomCache.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class ClearCustomCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cache:custom';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clears the cache for e.g. HTML purifier.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $FileSystem = new Filesystem();
+        $FileSystem->cleanDirectory(storage_path('app/purifier'));
+    }
+}
