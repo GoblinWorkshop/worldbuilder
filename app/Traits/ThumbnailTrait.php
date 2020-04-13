99e04f1b4e62e216ded9de08fe23abf79c522a47
Mathieu <mathieu@vormkracht10.nl>
2020-04-13 14:38:01 +0200

    Add empty image as fallback

--- app/Traits/ThumbnailTrait.php
+++ app/Traits/ThumbnailTrait.php
@@ -12,8 +12,17 @@ trait ThumbnailTrait
      * @return string the <img> tag or empty string in case of errors
      */
     public function thumbnail($width = null, $height = null, $attributes = []) {
+
+        $attributesHtml = '';
+        foreach ($attributes as $key => $value) {
+            $attributesHtml .= ' '. $key .'="'. $value .'"';
+        }
+
         if (empty($this->attributes['filename'])) {
-            return '';
+            if (isset($attributes['returnUrl'])) {
+                return asset('img/empty.png');
+            }
+            return '<img src="'. asset('img/empty.png') .'"'. $attributesHtml .' />';
         }
         if ($width !== null && ($width < 0 || $width > 1920)) {
             $width = 200;
@@ -31,10 +40,6 @@ trait ThumbnailTrait
         $tmpFilename = md5($type . $width . $height . $this->attributes['filename']) . '.'. $this->file_extension;
         $path = public_path('cache') . '/';
         $tmpFilepath = $path . $tmpFilename;
-        $attributesHtml = '';
-        foreach ($attributes as $key => $value) {
-            $attributesHtml .= ' '. $key .'="'. $value .'"';
-        }
         if (is_file($tmpFilepath)) {
             if (isset($attributes['returnUrl'])) {
                 return asset('/cache/'. $tmpFilename);
@@ -45,7 +50,15 @@ trait ThumbnailTrait
             $img = Image::make(storage_path('app') . '/' . $this->attributes['filename']);
         }
         catch (\Exception $e) {
-            return '';
+            
+
+            if (empty($this->attributes['filename'])) {
+                if (isset($attributes['returnUrl'])) {
+                    return asset('img/empty.png');
+                }
+                return '<img src="'. asset('img/empty.png') .'"'. $attributesHtml .' />';
+            }
+            
         }
         if ($width > 0 && $height > 0) {
             $img->fit($width, $height);
@@ -61,7 +74,13 @@ trait ThumbnailTrait
             }
             return '<img src="'. asset('/cache/'. $tmpFilename) .'"'. $attributesHtml .' />';
         }
-        return '';
+
+        if (empty($this->attributes['filename'])) {
+            if (isset($attributes['returnUrl'])) {
+                return asset('img/empty.png');
+            }
+            return '<img src="'. asset('img/empty.png') .'"'. $attributesHtml .' />';
+        }
     }
 
     /**
